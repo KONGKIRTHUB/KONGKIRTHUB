132cd3275ac45871484ded21ecf7c0ffddb33f25
KONGKIRTHUB <101847589+KONGKIRTHUB@users.noreply.github.com>
2022-06-30 08:50:26 +0700

    Update README.lua

--- README.lua
+++ README.lua
@@ -8,10 +8,12 @@ local tgls = serv:Channel("Mina")
 
 tgls:Toggle("Aimbot",false, function(bool)
 print(bool)
+-- function Aimbot
 function getplrsname() for i,v in pairs(game:GetChildren()) do if v.ClassName == "Players" then return v.Name end end end local players = getplrsname() local plr = game[players].LocalPlayer  coroutine.resume(coroutine.create(function() while wait(1) do coroutine.resume(coroutine.create(function() for _,v in pairs(game[players]:GetPlayers()) do if v.Name ~= plr.Name and v.Character then v.Character.RightUpperLeg.CanCollide = false v.Character.RightUpperLeg.Transparency = 10 v.Character.RightUpperLeg.Size = Vector3.new(21,21,21) v.Character.LeftUpperLeg.CanCollide = false v.Character.LeftUpperLeg.Transparency = 10 v.Character.LeftUpperLeg.Size = Vector3.new(21,21,21) v.Character.HeadHB.CanCollide = false v.Character.HeadHB.Transparency = 10 v.Character.HeadHB.Size = Vector3.new(21,21,21) v.Character.HumanoidRootPart.CanCollide = false v.Character.HumanoidRootPart.Transparency = 10 v.Character.HumanoidRootPart.Size = Vector3.new(21,21,21) end end end)) end end))
 
 
 end)
+-- function ESP
 tgls:Toggle("Esp",false, function(E)
 print(E)
 while wait() do
