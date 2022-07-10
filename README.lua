e5c222ad96d65d0c1470c9228121c1d2ee240083
KONGKIRTHUB <101847589+KONGKIRTHUB@users.noreply.github.com>
2022-07-10 18:34:32 +0700

    Update README.lua

--- README.lua
+++ README.lua
@@ -1,46 +1,331 @@
 local DiscordLib = loadstring(game:HttpGet"https://raw.githubusercontent.com/kickTh/New-Ui/main/discord%20lib%20(1).txt")()
 
-local win = DiscordLib:Window("KONGKIRT HUB | Arsenal")
+local win = DiscordLib:Window("discord library")
 
-local serv = win:Server("KONGKIRT HUB", "")
+local serv = win:Server("Preview", "http://www.roblox.com/asset/?id=6031075938")
 
-local tgls = serv:Channel("Mina")
+local btns = serv:Channel("Buttons")
 
-tgls:Toggle("Aimbot",false, function(bool)
-print(bool)
--- function Aimbot
-function getplrsname() for i,v in pairs(game:GetChildren()) do if v.ClassName == "Players" then return v.Name end end end local players = getplrsname() local plr = game[players].LocalPlayer  coroutine.resume(coroutine.create(function() while wait(1) do coroutine.resume(coroutine.create(function() for _,v in pairs(game[players]:GetPlayers()) do if v.Name ~= plr.Name and v.Character then v.Character.RightUpperLeg.CanCollide = false v.Character.RightUpperLeg.Transparency = 10 v.Character.RightUpperLeg.Size = Vector3.new(21,21,21) v.Character.LeftUpperLeg.CanCollide = false v.Character.LeftUpperLeg.Transparency = 10 v.Character.LeftUpperLeg.Size = Vector3.new(21,21,21) v.Character.HeadHB.CanCollide = false v.Character.HeadHB.Transparency = 10 v.Character.HeadHB.Size = Vector3.new(21,21,21) v.Character.HumanoidRootPart.CanCollide = false v.Character.HumanoidRootPart.Transparency = 10 v.Character.HumanoidRootPart.Size = Vector3.new(21,21,21) end end end)) end end))
-
-
+btns:Toggle("Kill all", function()
+DiscordLib:Notification("Notification", "Killed everyone!", "Okay!")
 end)
--- function ESP
-tgls:Toggle("Esp",false, function(E)
-print(E)
-while wait() do
-     pcall(function()
-       for i,v in pairs(game.Players:GetChildren()) do
-            if not v.Character.Head:FindFirstChild("ESP") then
-                local BillboardGui = Instance.new("BillboardGui")
-                local TextLabel = Instance.new("TextLabel")
-                BillboardGui.Parent = v.Character.Head
-                BillboardGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-                BillboardGui.Active = true
-                BillboardGui.Name = "ESP"
-                BillboardGui.AlwaysOnTop = true
-                BillboardGui.LightInfluence = 1.000
-                BillboardGui.Size = UDim2.new(0, 200, 0, 50)
-                BillboardGui.StudsOffset = Vector3.new(0, 2.5, 0)
-                TextLabel.Parent = BillboardGui
-                TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-                TextLabel.BackgroundTransparency = 1.000
-                TextLabel.Size = UDim2.new(0, 200, 0, 50)
-                TextLabel.Font = Enum.Font.GothamBold
-                TextLabel.Text = v.Name
-                TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
-                TextLabel.TextScaled = true
-                TextLabel.TextSize = 14.000
-                TextLabel.TextStrokeTransparency = 0.000
-                TextLabel.TextWrapped = true
+
+btns:Seperator()
+
+btns:Button("Get max level", function()
+DiscordLib:Notification("Notification", "Max level!", "Okay!")
+end)
+
+local tgls = serv:Channel("Toggles")
+
+tgls:Toggle("Auto-Farm",false, function(bool)
+print(bool)
+end)
+
+local sldrs = serv:Channel("Sliders")
+
+local sldr = sldrs:Slider("Slide me!", 0, 1000, 400, function(t)
+print(t)
+end)
+
+sldrs:Button("Change to 50", function()
+sldr:Change(50)
+end)
+
+local drops = serv:Channel("Dropdowns")
+
+
+local drop = drops:Dropdown("Pick me!",{"Option 1","Option 2","Option 3","Option 4","Option 5"}, function(bool)
+print(bool)
+end)
+
+drops:Button("Clear", function()
+drop:Clear()
+end)
+
+drops:Button("Add option", function()
+drop:Add("Option")
+end)
+
+local clrs = serv:Channel("Colorpickers")
+
+clrs:Colorpicker("ESP Color", Color3.fromRGB(255,1,1), function(t)
+print(t)
+end)
+
+local textbs = serv:Channel("Textboxes")
+
+textbs:Textbox("Gun power", "Type here!", true, function(t)
+print(t)
+end)
+
+local lbls = serv:Channel("Labels")
+
+lbls:Label("This is just a label.")
+
+local bnds = serv:Channel("Binds")
+
+bnds:Bind("Kill bind", Enum.KeyCode.RightShift, function()
+print("Killed everyone!")
+end)
+
+serv:Channel("by dawid#7205")
+
+
+win:Server("Main", "http://www.roblox.com/asset/?id=6031075938")
+btns = serv:Channel("Main")
+
+btns:Toggle("Auto Farm",false, function(bool)
+
+_G.FARM = bool -- true = ON  /  false = OFF
+_G.MON = "Bandit [Lv. 5]" -- NAME MONSTER
+_G.TOOL = "Combat" -- Weapon name
+_G.FARMMETHOD = 1 -- MODE FARM (1/3)
+_G.HideHB = true -- HIDEHITBOX
+
+
+-- =[[ FUNCTION ]]= --
+
+
+
+function EquipTool() -- WEAPON EQUIP
+    for i,v in pairs(game.Players.LocalPlayer.Backpack:GetChildren()) do
+        if v.name == _G.TOOL then
+            v.Parent = game.Players.LocalPlayer.Character
+        end
+    end
+    end
+
+
+function quest() -- CHECK QUESY AND AUTO QUEST
+    local MYLEVEL = game:GetService("Players").LocalPlayer.Data.Level.Value
+if game:GetService("Players").LocalPlayer.PlayerGui.Main.Quest.Visible == true then
+    
+    elseif MYLEVEL == 1 or MYLEVEL <= 9 then
+
+local args = {
+    [1] = "StartQuest",
+    [2] = "BanditQuest1",
+    [3] = 1
+}
+
+game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer(unpack(args))
+
+    
+
+end
+
+end
+
+
+function attack() -- AUTO ATTACK
+    game:GetService'VirtualUser':CaptureController()
+game:GetService'VirtualUser':Button1Down(Vector2.new(1280, 672))
+end
+
+
+function hitbox() -- HITBOX
+    local enamie = game:GetService("Workspace").Enemies:GetChildren()
+    
+    for i,v in pairs(enamie) do
+        v.HumanoidRootPart.Size = Vector3.new(0,0,0)
+        v.HumanoidRootPart.CanCollide = true
+        if _G.HideHB == true then
+        v.HumanoidRootPart.Transparency = 1
+        elseif _G.HideHB == false then
+            v.HumanoidRootPart.Transparency = 0.9
+            end
+        end
+    
+    end
+
+function TPTOMON() -- FARM MODE
+if game:GetService("Workspace").Enemies[_G.MON].Humanoid.Health == 0 then
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(1062.7824707031, 16.516611099243, 1539.5036621094)
+elseif _G.FARMMETHOD == 1 then
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Enemies[_G.MON].HumanoidRootPart.CFrame * CFrame.new(0,0,20)
+
+elseif _G.FARMMETHOD == 2 then
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Enemies[_G.MON].HumanoidRootPart.CFrame * CFrame.new(0,30,0)
+elseif    _G.FARMMETHOD == 3 then
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Enemies[_G.MON].HumanoidRootPart.CFrame * CFrame.new(0,-10,0)
+        end
+
+
+end
+
+
+
+-- =[[ Autofarm ]]= --
+
+while _G.FARM do  wait()
+    print("SHARK X HUB | NO 1") -- DON'T DELETE ME :angry:
+    EquipTool()
+    TPTOMON()
+    hitbox()
+    attack()
+    quest()
+    end
+end
+
+-- =[[ Autofarm Setting ]]= --
+
+
+
+_G.FARM = bool -- true = ON  /  false = OFF
+_G.MON = "Monkey [Lv. 9]" -- NAME MONSTER
+_G.TOOL = "Combat" -- Weapon name
+_G.FARMMETHOD = 1 -- MODE FARM (1/3)
+_G.HideHB = false -- HIDEHITBOX
+
+
+-- =[[ FUNCTION ]]= --
+
+
+
+function EquipTool() -- WEAPON EQUIP
+    for i,v in pairs(game.Players.LocalPlayer.Backpack:GetChildren()) do
+        if v.name == _G.TOOL then
+            v.Parent = game.Players.LocalPlayer.Character
+        end
+    end
+    end
+
+
+function quest() -- CHECK QUESY AND AUTO QUEST
+    local MYLEVEL = game:GetService("Players").LocalPlayer.Data.Level.Value
+if game:GetService("Players").LocalPlayer.PlayerGui.Main.Quest.Visible == true then
+    
+    elseif MYLEVEL == 1 or MYLEVEL <= 9 then
+
+local args = {
+    [1] = "StartQuest",
+    [2] = "BanditQuest1",
+    [3] = 1
+}
+
+game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer(unpack(args))
+
+    
+
+end
+
+end
+
+
+function attack() -- AUTO ATTACK
+    game:GetService'VirtualUser':CaptureController()
+game:GetService'VirtualUser':Button1Down(Vector2.new(1280, 672))
+end
+
+
+function hitbox() -- HITBOX
+    local enamie = game:GetService("Workspace").Enemies:GetChildren()
+    
+    for i,v in pairs(enamie) do
+        v.HumanoidRootPart.Size = Vector3.new(0,0,0)
+        v.HumanoidRootPart.CanCollide = true
+        if _G.HideHB == true then
+        v.HumanoidRootPart.Transparency = 1
+        elseif _G.HideHB == false then
+            v.HumanoidRootPart.Transparency = 0.5
+            end
+        end
+    
+    end
+
+function TPTOMON() -- FARM MODE
+if game:GetService("Workspace").Enemies[_G.MON].Humanoid.Health == 0 then
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(1062.7824707031, 16.516611099243, 1539.5036621094)
+elseif _G.FARMMETHOD == 1 then
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Enemies[_G.MON].HumanoidRootPart.CFrame * CFrame.new(0,0,20)
+
+elseif _G.FARMMETHOD == 2 then
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Enemies[_G.MON].HumanoidRootPart.CFrame * CFrame.new(0,30,0)
+elseif    _G.FARMMETHOD == 3 then
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Enemies[_G.MON].HumanoidRootPart.CFrame * CFrame.new(0,-10,0)
+        end
+end
+while _G.FARM do  wait()
+    print("SHARK X HUB | NO 1") -- DON'T DELETE ME :angry:
+    EquipTool()
+    TPTOMON()
+    hitbox()
+    attack()
+    quest()
+    end
+
+btns:Toggle("Fast Attack",false, function(bool)
+    
+(getgenv()).Config = {
+ ["FastAttack"] = bool,
+ ["ClickAttack"] = bool
+} 
+
+coroutine.wrap(function()
+local StopCamera = require(game.ReplicatedStorage.Util.CameraShaker)StopCamera:Stop()
+    for v,v in pairs(getreg()) do
+        if typeof(v) == "function" and getfenv(v).script == game:GetService("Players").LocalPlayer.PlayerScripts.CombatFramework then
+             for v,v in pairs(debug.getupvalues(v)) do
+                if typeof(v) == "table" then
+                    spawn(function()
+                        game:GetService("RunService").RenderStepped:Connect(function()
+                            if getgenv().Config['FastAttack'] then
+                                 pcall(function()
+                                     v.activeController.timeToNextAttack = -(math.huge^math.huge^math.huge)
+                                     v.activeController.attacking = false
+                                     v.activeController.increment = 4
+                                     v.activeController.blocking = false   
+                                     v.activeController.hitboxMagnitude =150
+    		                         v.activeController.humanoid.AutoRotate = true
+    	                      	     v.activeController.focusStart = 0
+    	                      	     v.activeController.currentAttackTrack = 0
+                                     sethiddenproperty(game:GetService("Players").LocalPlayer, "SimulationRaxNerous", math.huge)
+                                 end)
+                             end
+                         end)
+                    end)
+                end
+            end
+        end
+    end
+end)();
+
+spawn(function()
+    game:GetService("RunService").RenderStepped:Connect(function()
+        if getgenv().Config['ClickAttack'] then
+             pcall(function()
+                game:GetService'VirtualUser':CaptureController()
+			    game:GetService'VirtualUser':Button1Down(Vector2.new(0,1,0,1))
+            end)
+        end
+    end)
+    end)
+end)
+
+btns:Toggle("bing mon",false, function(bool)
+    _G.bringmob = true
+while _G.bringmob do wait()
+    pcall(function()
+for i,v in pairs(game:GetService("Workspace").Enemies:GetChildren()) do
+for x,y in pairs(game:GetService("Workspace").Enemies:GetChildren()) do
+if v.Name == "Bandit [Lv. 5]" then
+    if y.Name == "Bandit [Lv. 5]" then
+   v.HumanoidRootPart.CFrame = y.HumanoidRootPart.CFrame
+   v.HumanoidRootPart.Size = Vector3.new(60,60,60)
+   y.HumanoidRootPart.Size = Vector3.new(60,60,60)
+   v.HumanoidRootPart.Transparency = 1
+   v.HumanoidRootPart.CanCollide = false
+   y.HumanoidRootPart.CanCollide = false
+   v.Humanoid.WalkSpeed = 0
+   y.Humanoid.WalkSpeed = 0
+   v.Humanoid.JumpPower = 0
+   y.Humanoid.JumpPower = 0
+   if sethiddenproperty then
+     sethiddenproperty(game.Players.LocalPlayer, "SimulationRadius", math.huge)
+end
+end
+end
 end
 end
 end)
